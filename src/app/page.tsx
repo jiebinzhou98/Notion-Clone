21e8fe2e0177a3448a82d635df455b776d56e564
jiebinzhou98 <benzhou98@outlook.com>
2025-04-30 22:55:11 -0400

    install lucide react,and completing the homepage UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
 import TypewriterTitle from "@/components/TypewriterTitle";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import {ArrowRight} from "lucide-react";
 
 export default function Home() {
   return (
@@ -11,6 +14,14 @@ export default function Home() {
         <h2 className="font-semibold text-3xl text-center text-slate-700">AI Powered
           <TypewriterTitle />
         </h2>
+        <div className="mt-8"></div>
+        <div className="flex justify-center">
+        <Link href="/dashboard">
+          <Button className="bg-green-600">Get Started
+            <ArrowRight className="ml-2 w-5 h-5 " strokeWidth={3}/>
+          </Button>
+        </Link>
+        </div>
       </div>
     </div>
   );
