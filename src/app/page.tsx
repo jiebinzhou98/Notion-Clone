1303f99a0849affe334abbd43b310997d62930f4
jiebinzhou98 <benzhou98@outlook.com>
2025-04-30 22:47:01 -0400

    installing typewriter and implement to the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
+import TypewriterTitle from "@/components/TypewriterTitle";
+
 export default function Home() {
   return (
     <div className="bg-gradient-to-r min-h-screen grainy from-rose-100 to-teal-100">
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 translate-y-1/2">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
         <h1 className="font-semibold text-7xl text-center">
-          AI note taking assistant
+          AI <span className="text-green-600 font-bold">note taking</span>{' '} assistant
         </h1>
+        <div className="mt-4"></div>
+        <h2 className="font-semibold text-3xl text-center text-slate-700">AI Powered
+          <TypewriterTitle />
+        </h2>
       </div>
     </div>
   );
