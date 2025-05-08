83413664c718dce6db0d596b675f613c1f5e055a
jiebinzhou98 <benzhou98@outlook.com>
2025-05-08 12:44:17 -0400

    update notebook setting and fianl UI

--- src/app/notebook/[noteId]/page.tsx
+++ src/app/notebook/[noteId]/page.tsx
@@ -4,6 +4,7 @@ import { db } from '@/lib/db'
 import { $notes } from '@/lib/db/schema'
 import { and, eq } from 'drizzle-orm'
 import { redirect } from 'next/navigation'
+import { clerkClient } from '@/lib/clerk-server'
 import Link from 'next/link'
 import { Button } from '@/components/ui/button'
 
@@ -28,14 +29,26 @@ const NotebookPage = async ({ params: { noteId } }: Props) => {
     if (notes.length !== 1) {
         return redirect('/dashboard')
     }
+    const user = await clerkClient.users.getUser(userId)
     const note = notes[0]
     return (
         <div className='min-h-screen grainy p-8'>
             <div className='max-w-4xl mx-auto'>
                 <div className='border shadow-xl border-stone-200 rounded-lg p-4 flex items-center'>
                 <Link href='/dashboard'>
-                    <Button className='bg-green-600'>Back</Button>
+                    <Button className='bg-green-600' size='sm'>Back</Button>
                 </Link>
+                    <div className='w-3'></div>
+                    <span className='font-semibold'>
+                        {user.firstName} {user.lastName}
+                    </span>
+                    <span className='inline-block mx-1'>/</span>
+                    <span className='text-stone-500 font-semibold'>{note.name}</span>
+                    <div className='ml-auto'>DELETE BUTTON</div>
+                </div>
+                <div className='h-4'></div>
+                <div className='border-stone-200 shadow-xl border rounded-lg px-16 py-8 w-full'>
+
                 </div>
             </div>
         </div>
