f9971bc83a6acb937ed53e45314589790b69d244
jiebinzhou98 <benzhou98@outlook.com>
2025-05-11 14:36:38 -0400

    complete set up for the firebase

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getStorage } from "firebase/storage"
+import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,3 +18,19 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const storage = getStorage(app)
+
+export async function uploadFileToFirebase(image_url: string, name: string) {
+    try{
+        const response = await fetch(image_url)
+        const buffer = await response.arrayBuffer()
+        const file_name = name.replace(' ','') + Date.now + '.jpeg'
+        const storageRef = ref(storage, file_name)
+        await uploadBytes(storageRef, buffer, {
+            contentType: 'image/jpeg'
+        })
+        const firebase_url = await getDownloadURL(storageRef)
+        return firebase_url
+    }catch(error){
+        console.error(error)
+    }
+}
